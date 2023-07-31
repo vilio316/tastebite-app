ccf9385e1e275e63a045f29b4e5ae351fbc8b136
vilio316 <atembarry12@gmail.com>
2023-07-31 15:59:57 +0100

    Fin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,15 +24,15 @@ function Header(){
             </span>
     </div>
     <div ref={visible_div} style={{backgroundColor:"black", opacity: "0.85", height: "100%", width: "100%", position:"absolute", top: 0, display:"none"}}>
-        <div className="grid search_div" style={{height: "10rem"}}>
+        <div className="grid search_div" style={{height: "10%"}}>
         <label>
             <input ref={input} type="text" id={"search-input"} placeholder={`Search Here...`} style={{width: "90%", fontSize: "7.5rem", padding: "0.5rem 0", marginTop: "10%",color:"white", backgroundColor:"black", textIndent:"2.5rem", fontWeight:"bold", outline: "none",borderRadius:"1.5rem", border: "0.25rem solid "}}/>
         </label>
-        <div className="grid" style={{gridTemplateColumns: "auto auto"}}>
-        <button style={{borderRadius: "1rem", placeSelf:"center", border: "none", color: "white", backgroundColor:"#ff642f"}}onClick={()=> {
+        <div className="grid" style={{gridTemplateColumns: "auto auto", marginTop: "10%"}}>
+        <button class=" search_button"style={{borderRadius: "1rem", placeSelf:"center", border: "none", color: "white", backgroundColor:"#ff642f"}}onClick={()=> {
             changeState(input.current.value);
         }}><a href={`/recipes/${search_input}`}>Search</a></button>
-        <span ref={closer} style={{fontSize: "3rem", color: "white", fontWeight: "bold", borderRadius: "50%", backgroundColor:"red", height:"7.5rem", width: "7.5rem", display:"grid", textAlign:"center", placeContent:"center"}} onClick={()=> {
+        <span ref={closer} style={{fontSize: "3rem", color: "white", fontWeight: "bold", borderRadius: "50%", backgroundColor:"red", height:"6rem", width: "6rem", display:"grid", textAlign:"center", placeContent:"center"}} onClick={()=> {
             let james = visible_div.current;
             james.style.display = "none";
         }}>X</span>
