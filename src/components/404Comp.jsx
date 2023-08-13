c136b8b11470f678c9c11b8311215483c9832981
vilio316 <atembarry12@gmail.com>
2023-08-13 20:28:37 +0100

    Fin

--- src/components/404Comp.jsx
+++ src/components/404Comp.jsx
@@ -8,7 +8,7 @@ export default function CompErr(){
         <>
         <div style={{display: "grid"}}>
             <p style={{fontSize: "3.5rem", textAlign:"center"}}>Error 404: Not Found</p>
-            <img src={joestar_gif} style={{width: "90%", placeSelf:"center", borderRadius:"2rem"}}/>
+            <img src={joestar_gif} style={{width: "30%", placeSelf:"center", borderRadius:"2rem"}}/>
             <p style={{fontSize: "1.5rem", textIndent:"2.5rem"}}>Oops, it looks like you've taken a wrong turn somewhere. We apologize for the inconvenience you've encountered. But don't worry, we're here to help! Please check the URL or try using our site navigation to find what you're looking for. If you believe there's an issue with our website, we'd truly appreciate it if you could let us know, so we can fix it promptly.</p>
         </div>
         </>
